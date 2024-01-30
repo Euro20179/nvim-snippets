a3e9326633c8b05ade29444decaddcb575b29b29
euro20179 <sandollar2020@gmail.com>
2024-01-30 08:28:21 -0800

    feat: ability to add snippets at runtime

--- lua/nvim-snippets/init.lua
+++ lua/nvim-snippets/init.lua
@@ -21,44 +21,65 @@ snippets.registry = {}
 ---@type table<string, string>
 snippets.prefix_lookup = {}
 
+---@private
+---@type table<string, table<string, Snippet>>
+---This will be in the format of ft -> snippet-name -> snippet
+snippets.runtime_snippets = {}
+
 ---@type fun(filetype?: string): table<string, table>|nil
 function snippets.load_snippets_for_ft(filetype)
-	if snippets.utils.is_filetype_ignored(filetype) then
-		return nil
-	end
+    if snippets.utils.is_filetype_ignored(filetype) then
+        return nil
+    end
 
-	local global_snippets = snippets.utils.get_global_snippets()
-	local extended_snippets = snippets.utils.get_extended_snippets(filetype)
-	local ft_snippets = snippets.utils.get_snippets_for_ft(filetype)
-	snippets.loaded_snippets = vim.tbl_deep_extend("force", {}, global_snippets, extended_snippets, ft_snippets)
+    local global_snippets = snippets.utils.get_global_snippets()
+    local extended_snippets = snippets.utils.get_extended_snippets(filetype)
+    local ft_snippets = snippets.utils.get_snippets_for_ft(filetype)
+    local runtime_ft_snippets = snippets.runtime_snippets[filetype] or {}
+    snippets.loaded_snippets = vim.tbl_deep_extend("force", {}, global_snippets, extended_snippets, ft_snippets,
+        runtime_ft_snippets)
 
-	for key, snippet in pairs(snippets.loaded_snippets) do
-		snippets.prefix_lookup[snippet.prefix] = key
-	end
-	return snippets.loaded_snippets
+    for key, snippet in pairs(snippets.loaded_snippets) do
+        snippets.prefix_lookup[snippet.prefix] = key
+    end
+    return snippets.loaded_snippets
 end
 
 ---@return table<string, Snippet>
 function snippets.get_loaded_snippets()
-	return snippets.loaded_snippets
+    return snippets.loaded_snippets
+end
+
+---@param filetype string
+---@param snippet table<string, Snippet>
+---@return boolean
+---Returns true if it successfuly added the snippet, otherwise false
+function snippets.add_snippet(filetype, snippet)
+    local runtime_ft_snippets = snippets.runtime_snippets[filetype] or {}
+    local e, result = pcall(vim.tbl_deep_extend, "error", runtime_ft_snippets, snippet)
+    if not e then
+        return e
+    end
+    snippets.runtime_snippets[filetype] = result
+    return true
 end
 
 ---@param opts? table  -- Make a better type for this
 function snippets.setup(opts)
-	snippets.config.new(opts)
-	if snippets.config.get_option("friendly_snippets") then
-		snippets.utils.load_friendly_snippets()
-	end
+    snippets.config.new(opts)
+    if snippets.config.get_option("friendly_snippets") then
+        snippets.utils.load_friendly_snippets()
+    end
 
-	snippets.utils.register_snippets()
+    snippets.utils.register_snippets()
 
-	if snippets.config.get_option("create_autocmd") then
-		snippets.utils.create_autocmd()
-	end
+    if snippets.config.get_option("create_autocmd") then
+        snippets.utils.create_autocmd()
+    end
 
-	if snippets.config.get_option("create_cmp_source") then
-		snippets.utils.register_cmp_source()
-	end
+    if snippets.config.get_option("create_cmp_source") then
+        snippets.utils.register_cmp_source()
+    end
 end
 
 return snippets
